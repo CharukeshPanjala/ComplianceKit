a43cf563a516df671dc0937f7f2195a40bef4a4c
CharukeshPanjala <charukeshpanjala007@gmail.com>
2026-05-03 22:14:26 +0200

    fix(common): add proper type hints to log_config.py for mypy compatibility (COM-26)

--- packages/common/common/log_config.py
+++ packages/common/common/log_config.py
@@ -1,16 +1,17 @@
 import logging
 import sys
 import structlog
+from structlog.types import Processor
 
 
 def configure_logging(environment: str = "development") -> None:
-    shared_processors = [
-        structlog.contextvars.merge_contextvars,
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.add_log_level,
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.processors.StackInfoRenderer(),
-    ]
+    shared_processors: list[Processor] = [
+    structlog.contextvars.merge_contextvars,
+    structlog.stdlib.add_logger_name,
+    structlog.stdlib.add_log_level,
+    structlog.processors.TimeStamper(fmt="iso"),
+    structlog.processors.StackInfoRenderer(),
+]
 
     if environment == "production":
         processors = shared_processors + [
